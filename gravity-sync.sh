1f1aead6d4997fae12e5613241ff20e3a437e7b6
Michael Stanclift <mstanclift@vmware.com>
2020-06-01 13:30:43 -0500

    fi;

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -595,7 +595,7 @@ function validate_os_sshpass {
 			error_validate
 		elif hash dbclient 2>/dev/null
 		then
-			if [ "$SSH_CMD" = "dbclient" ]; then; echo ''; fi
+			if [ "$SSH_CMD" = "dbclient" ]; then; echo ''; fi;
 		timeout 5 ${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i '$HOME/${SSH_PKIF}' ${REMOTE_USER}@${REMOTE_HOST} 'exit' >/dev/null 2>&1
 			error_validate
 		fi
@@ -730,7 +730,7 @@ function md5_compare {
 
 	MESSAGE="Analyzing ${REMOTE_HOST} ${GRAVITY_FI}"
 	echo_stat
-	if [ "$SSH_CMD" = "dbclient" ]; then; echo ''; fi
+	if [ "$SSH_CMD" = "dbclient" ]; then; echo ''; fi;
 	primaryDBMD5=$(${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i "$HOME/${SSH_PKIF}" ${REMOTE_USER}@${REMOTE_HOST} "md5sum ${PIHOLE_DIR}/${GRAVITY_FI}")
 		error_validate
 	
