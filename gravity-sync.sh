95dd3d56347d304cd5ee38bcc7035e06fe075510
Michael Stanclift <vmstan@MS-MacBook.vmstan.net>
2020-07-07 15:19:59 -0500

    missing $

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -402,7 +402,7 @@ function smart_gs {
 		last_primaryDBMD5=$(sed "1q;d" ${LOG_PATH}/${HISTORY_MD5})
 		last_secondDBMD5=$(sed "2q;d" ${LOG_PATH}/${HISTORY_MD5})
 		last_primaryCLMD5=$(sed "3q;d" ${LOG_PATH}/${HISTORY_MD5})
-		last_secondCLMD5=$(sed "4q;d" ${LOG_PATH}/{HISTORY_MD5})
+		last_secondCLMD5=$(sed "4q;d" ${LOG_PATH}/{$HISTORY_MD5})
 	else
 		last_primaryDBMD5="0"
 		last_secondDBMD5="0"
