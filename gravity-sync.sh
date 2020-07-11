958aa017f86569055176cfcd8bd6f8e3e7a3b648
Michael Stanclift <vmstan@MS-MacBook.vmstan.net>
2020-07-10 22:33:31 -0500

    wrong variable

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -1509,14 +1509,14 @@ function task_automate {
 		MESSAGE="Invalid Frequency Range"
 		echo_fail
 		exit_nochange
-	elif [ $INPUT_AUTO_BACKUP-lt 1 ]
+	elif [ $INPUT_AUTO_BACKUP -lt 1 ]
 	then
 		MESSAGE="No Backup Automation Scheduled"
 		echo_warn
 	else
 		MESSAGE="Saving New Backup Automation"
 		echo_stat
-		(crontab -l 2>/dev/null; echo "* ${INPUT_AUTO_FREQ} * * * ${BASH_PATH} $HOME/${LOCAL_FOLDR}/${GS_FILENAME} backup >/dev/null 2>&1") | crontab -
+		(crontab -l 2>/dev/null; echo "* ${INPUT_AUTO_BACKUP} * * * ${BASH_PATH} $HOME/${LOCAL_FOLDR}/${GS_FILENAME} backup >/dev/null 2>&1") | crontab -
 			error_validate
 	fi
 
