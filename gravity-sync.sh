3a2eed97f6d9754c4761bf67dd1b1f84398605c5
Michael Stanclift <vmstan@Sovereign.vmstan.net>
2020-10-01 15:29:06 -0500

    2.2.2(1)

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -3,7 +3,7 @@ SCRIPT_START=$SECONDS
 
 # GRAVITY SYNC BY VMSTAN #####################
 PROGRAM='Gravity Sync'
-VERSION='2.2.1'
+VERSION='2.2.2'
 
 # Execute from the home folder of the user who owns it (ex: 'cd ~/gravity-sync')
 # For documentation or downloading updates visit https://github.com/vmstan/gravity-sync
