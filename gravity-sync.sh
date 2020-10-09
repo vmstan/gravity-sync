5d5987ee214f234730160a269156554a046e6fad
Michael Stanclift <vmstan@sovereign.vmstan.net>
2020-10-09 03:26:04 +0000

    Create gs-core file

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -66,72 +66,10 @@ source includes/gs-colors.sh
 
 # FUNCTION DEFINITIONS #######################
 
-# Import Settings
-function import_gs {
-	MESSAGE="Importing ${CONFIG_FILE} Settings"
-	echo -en "${STAT} $MESSAGE"
-	if [ -f $HOME/${LOCAL_FOLDR}/${CONFIG_FILE} ]
-	then
-	    source $HOME/${LOCAL_FOLDR}/${CONFIG_FILE}
-			error_validate
-			
-		# MESSAGE="Targeting ${REMOTE_USER}@${REMOTE_HOST}"
-		# echo_info
+# Core Functions
+source includes/gs-core.sh
 
-		# detect_ssh
-	else
-		echo_fail
-		
-		MESSAGE="${CONFIG_FILE} Missing"
-		echo_info
-
-		TASKTYPE='CONFIG'
-		config_generate
-	fi
-}
-
-# Invalid Tasks
-function task_invalid {
-	echo_fail
-	list_gs_arguments
-}
-
-# Error Validation
-function error_validate {
-	if [ "$?" != "0" ]
-	then
-	    echo_fail
-	    exit 1
-	else
-		echo_good
-	fi
-}
-
-# Standard Output
-function start_gs {
-	MESSAGE="${PROGRAM} ${VERSION} Executing"
-	echo_info
-	
-	import_gs
-
-	MESSAGE="Evaluating Arguments"
-	echo_stat
-
-	if [ "${ROOT_CHECK_AVOID}" != "1" ]
-	then
-		root_check
-	fi
-}
-
-function start_gs_noconfig {
-	MESSAGE="${PROGRAM} ${VERSION} Executing"
-	echo_info
-
-	MESSAGE="Evaluating Arguments"
-	echo_stat
-}
-
-# Gravity Core Functions
+# Gravity Replication Functions
 source includes/gs-compare.sh
 source includes/gs-pull.sh
 source includes/gs-push.sh
