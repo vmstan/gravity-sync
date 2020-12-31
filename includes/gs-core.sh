84be4ed2a35867e96d87ac20269d75e7b499293b
Michael Stanclift <vmstan@sovereign.local>
2020-12-31 15:56:23 +0000

    Move compare function to gs-core

--- includes/gs-core.sh
+++ includes/gs-core.sh
@@ -90,3 +90,19 @@ function start_gs_noconfig {
     MESSAGE="Evaluating Arguments"
     echo_stat
 }
+
+# Compare Task
+function task_compare {
+    TASKTYPE='COMPARE'
+    MESSAGE="${MESSAGE}: ${TASKTYPE} Requested"
+    echo_good
+    
+    show_target
+    validate_gs_folders
+    validate_ph_folders
+    validate_os_sshpass
+    
+    previous_md5
+    md5_compare
+    exit_withchange
+}
