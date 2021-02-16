bae9a0411f8b053d55ea52bd793422b63df6fa88
Michael Stanclift <vmstan@mstanclift-a03.local>
2021-02-16 18:33:52 +0000

    Compare updated

--- includes/gs-hashing.sh
+++ includes/gs-hashing.sh
@@ -34,13 +34,13 @@ function md5_compare {
             if ${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i "$HOME/${SSH_PKIF}" ${REMOTE_USER}@${REMOTE_HOST} test -e ${RIHOLE_DIR}/${CUSTOM_DNS}
             then
                 REMOTE_CUSTOM_DNS="1"
-                MESSAGE="Hashing remote DNS database"
+                MESSAGE="Hashing remote DNS file"
                 echo_stat
                 
                 primaryCLMD5=$(${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i "$HOME/${SSH_PKIF}" ${REMOTE_USER}@${REMOTE_HOST} "md5sum ${RIHOLE_DIR}/${CUSTOM_DNS} | sed 's/\s.*$//'")
                 error_validate
                 
-                MESSAGE="Hashing local DNS database"
+                MESSAGE="Hashing local DNS file"
                 echo_stat
                 secondCLMD5=$(md5sum ${PIHOLE_DIR}/${CUSTOM_DNS} | sed 's/\s.*$//')
                 error_validate
@@ -49,7 +49,7 @@ function md5_compare {
                 then
                     HASHMARK=$((HASHMARK+0))
                 else
-                    MESSAGE="Differenced DNS database detected"
+                    MESSAGE="Differenced ${CUSTOM_DNS} detected"
                     echo_warn
                     HASHMARK=$((HASHMARK+1))
                 fi
@@ -79,13 +79,13 @@ function md5_compare {
                 if ${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i "$HOME/${SSH_PKIF}" ${REMOTE_USER}@${REMOTE_HOST} test -e ${RNSMAQ_DIR}/${CNAME_CONF}
                 then
                     REMOTE_CNAME_DNS="1"
-                    MESSAGE="Hashing remote CNAME database"
+                    MESSAGE="Hashing remote CNAME extension"
                     echo_stat
                     
                     primaryCNMD5=$(${SSHPASSWORD} ${SSH_CMD} -p ${SSH_PORT} -i "$HOME/${SSH_PKIF}" ${REMOTE_USER}@${REMOTE_HOST} "md5sum ${RNSMAQ_DIR}/${CNAME_CONF} | sed 's/\s.*$//'")
                     error_validate
                     
-                    MESSAGE="Hashing local CNAME database"
+                    MESSAGE="Hashing local CNAME extension"
                     echo_stat
                     secondCNMD5=$(md5sum ${DNSMAQ_DIR}/${CNAME_CONF} | sed 's/\s.*$//')
                     error_validate
