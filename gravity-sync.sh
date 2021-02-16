53f8698d0bee32a848e6b2cdfe468069f7e27085
Michael Stanclift <vmstan@mstanclift-a03.local>
2021-02-16 17:34:09 +0000

    version update

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -3,7 +3,7 @@ SCRIPT_START=$SECONDS
 
 # GRAVITY SYNC BY VMSTAN #####################
 PROGRAM='Gravity Sync'
-VERSION='3.3.2'
+VERSION='3.3.3'
 
 
 # For documentation or downloading updates visit https://github.com/vmstan/gravity-sync
