5ef0e1e8cd7a806a4c30d245d7ae9eeb816c83b3
Michael Stanclift <vmstan@ms-constitution.local>
2021-04-05 22:05:52 +0000

    Add backup timeout customization

--- gravity-sync.sh
+++ gravity-sync.sh
@@ -3,7 +3,7 @@ SCRIPT_START=$SECONDS
 
 # GRAVITY SYNC BY VMSTAN #####################
 PROGRAM='Gravity Sync'
-VERSION='3.4.0'
+VERSION='3.4.1'
 
 
 # For documentation or downloading updates visit https://github.com/vmstan/gravity-sync
@@ -53,6 +53,7 @@ ROOT_CHECK_AVOID='0'				# replace in gravity-sync.conf to overwrite
 
 # Backup Customization
 BACKUP_RETAIN='3'					# replace in gravity-sync.conf to overwrite
+BACKUP_TIMEOUT='60'                 # replace in gravity-sync.conf to overwrite
 
 # SSH Customization
 SSH_PORT='22' 						# default SSH port
