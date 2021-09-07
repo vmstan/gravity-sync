f14807637b0be42f05fe616fbbe9e2dda623384a
Michael Stanclift <mstanclift@vmware.com>
2021-09-07 11:43:50 -0500

    MAAAAATH

--- includes/gs-exit.sh
+++ includes/gs-exit.sh
@@ -23,7 +23,7 @@ function exit_nochange {
 ## Changes Made
 function exit_withchange {
     SCRIPT_END=$SECONDS
-    SCRIPT_RUN=$(expr $SCRIPT_END-$SCRIPT_START
+    SCRIPT_RUN=$(expr $SCRIPT_END-$SCRIPT_START)
     
     if [ "${TASKTYPE}" == "" ]
     then
