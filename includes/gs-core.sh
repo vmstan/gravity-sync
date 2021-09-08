88672627f8bbd58fed5749c53f936fa9eac2ec47
Michael Stanclift <mstanclift@vmware.com>
2021-09-08 10:36:43 -0500

    Move compare task to core, delete compare file

--- includes/gs-core.sh
+++ includes/gs-core.sh
@@ -150,3 +150,27 @@ function ph_type {
         RH_EXEC="sudo ${RODMAN_BIN} exec ${ROCKER_CON} pihole"
     fi
 }
+
+## Compare Task
+function task_compare {
+    TASKTYPE='COMPARE'
+    MESSAGE="${MESSAGE}: ${TASKTYPE}"
+    echo_good
+    
+    show_target
+    validate_gs_folders
+    validate_ph_folders
+    
+    if [ "${INCLUDE_CNAME}" == "1" ]
+    then
+        validate_dns_folders
+    fi
+    
+    validate_os_sshpass
+    
+    previous_md5
+    md5_compare
+    backup_cleanup
+    
+    exit_withchange
+}
