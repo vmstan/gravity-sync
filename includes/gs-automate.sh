5e494384a429038720ccff7d2aceef64223ceefc
Michael Stanclift <mx@vmstan.com>
2022-03-29 10:42:17 -0500

    Remove legacy cron code from automate function

--- includes/gs-automate.sh
+++ includes/gs-automate.sh
@@ -19,47 +19,6 @@ function task_automate {
         clear_cron
     fi
     
-    MESSAGE="${UI_AUTO_CRON_DISPLAY_FREQ}"
-    echo_info
-    
-    if [[ $1 =~ ^[0-9][0-9]?$ ]]
-    then
-        INPUT_AUTO_FREQ=$1
-    else
-        MESSAGE="${UI_AUTO_CRON_SELECT_FREQ}"
-        echo_need
-        read INPUT_AUTO_FREQ
-        INPUT_AUTO_FREQ="${INPUT_AUTO_FREQ:-15}"
-    fi
-    
-    if [ $INPUT_AUTO_FREQ == 5 ] || [ $INPUT_AUTO_FREQ == 10 ] || [ $INPUT_AUTO_FREQ == 15 ] || [ $INPUT_AUTO_FREQ == 30 ]
-    then
-        if [ $CRON_EXIST == 1 ]
-        then
-            clear_cron
-        fi
-
-        path_fix
-        
-        MESSAGE="${UI_AUTO_CRON_SAVING}"
-        echo_stat
-        (crontab -l 2>/dev/null; echo "*/${INPUT_AUTO_FREQ} * * * * ${BASH_PATH} ${LOCAL_FOLDR}/${GS_FILENAME} smart > ${LOG_PATH}/${CRONJOB_LOG}") | crontab -
-        error_validate
-    elif [ $INPUT_AUTO_FREQ == 0 ]
-    then
-        if [ $CRON_EXIST == 1 ]
-        then
-            clear_cron
-        else
-            MESSAGE="${UI_AUTO_CRON_DISABLED}"
-            echo_warn
-        fi
-    else
-        MESSAGE="${UI_INVALID_SELECTION}"
-        echo_fail
-        exit_nochange
-    fi
-    
     exit_withchange
 }
 
